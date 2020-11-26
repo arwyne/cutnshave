e67ec3b2831cd691970526ab4fc24f4bda94a07d
Arwyne De Guzman <arwynedeguzman@gmail.com>
2020-11-26 22:47:44 +0800

    add temporary header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { BrowserRouter, Route } from "react-router-dom"
+import Header from "./components/Header"
+import HomeScreen from "./screens/HomeScreen"
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Route path="/" component={HomeScreen} exact />
     </BrowserRouter>
   )
